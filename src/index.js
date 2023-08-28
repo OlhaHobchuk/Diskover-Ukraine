a47704c2a91ad0e084007baaafac424f59281ce4
OlhaHobchuk <111050433+OlhaHobchuk@users.noreply.github.com>
2023-08-28 11:43:08 +0300

    Створює розмітку і стилізацію до сайту

---
+++ src/index.js
@@ -0,0 +1,55 @@
+const swiper = new Swiper('.swiper', {
+  direction: 'horizontal',
+  loop: true,
+  navigation: {
+    nextEl: '.nextEl',
+    prevEl: '.prevEl',
+  },
+});
+
+// const swiperEl = new Swiper('.swiper', {
+//   direction: 'horizontal',
+//   loop: true,
+
+//   navigation: {
+//     nextEl: '.nextElGallery',
+//     prevEl: '.prevElGallery',
+//   },
+// });
+
+// mobile-menu
+
+(() => {
+  const mobileMenu = document.querySelector('.js-menu-container');
+  const openMenuBtn = document.querySelector('.js-open-menu');
+  const closeMenuBtn = document.querySelector('.js-close-menu');
+  const navLink = document.querySelectorAll('.mobile-menu__navigation-link');
+
+  const toggleMenu = () => {
+    const isMenuOpen =
+      openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
+    openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
+    mobileMenu.classList.toggle('is-open');
+
+    const scrollLockMethod = !isMenuOpen
+      ? 'disableBodyScroll'
+      : 'enableBodyScroll';
+    bodyScrollLock[scrollLockMethod](document.body);
+  };
+
+  openMenuBtn.addEventListener('click', toggleMenu);
+  closeMenuBtn.addEventListener('click', toggleMenu);
+  navLink.forEach(item => {
+    item.addEventListener('click', () => {
+      mobileMenu.classList.remove('is-open');
+    });
+  });
+
+  // Close the mobile menu on wider screens if the device orientation changes
+  window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
+    if (!e.matches) return;
+    mobileMenu.classList.remove('is-open');
+    openMenuBtn.setAttribute('aria-expanded', false);
+    bodyScrollLock.enableBodyScroll(document.body);
+  });
+})();
